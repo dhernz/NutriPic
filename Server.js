3a3442ea57df634af9efeb66e622ba35bdbf3b1b
Doris Hernandez <d.hernz@gmail.com>
2015-08-04 16:32:18 -0700

    added some styling

--- Server.js
+++ Server.js
@@ -35,6 +35,6 @@ app.post('/api/photo', function (req, res) {
     }
 });
 
-app.listen(8080, function () {
-    // console.log("Working on port 8080");
-});
+
+var port = process.env.PORT || 8080 ;
+app.listen(port);
